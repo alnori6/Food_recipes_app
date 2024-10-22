081cf0d042c09a33bcb714e97bc070e8ee0024a9
noora amer <aoora6@gmail.com>
2024-10-22 10:11:09 +0300

    still fixing the photo picker

---
+++ Food_recipes/Food_recipes/newRecipe_page.swift
@@ -0,0 +1,18 @@
+//
+//  newRecipe_page.swift
+//  Food_recipes
+//
+//  Created by Noori on 20/10/2024.
+//
+
+import SwiftUI
+
+struct newRecipe_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newRecipe_page()
+}
