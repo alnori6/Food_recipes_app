cd341c7d414e1bcdef802c528a885d0c0fd588dc
noora amer <aoora6@gmail.com>
2024-10-29 01:27:42 +0300

    almost done with the edit view and the delete , hopefully i will be done in time

---
+++ Food_recipes/Food_recipes/View/RecipeEdit_page.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeEdit_page.swift
+//  Food_recipes
+//
+//  Created by Noori on 29/10/2024.
+//
+
+import SwiftUI
+
+struct RecipeEdit_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeEdit_page()
+}
